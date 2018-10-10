8c62491dedd25e5f3bc1a42df7057da4cf8fd9d2
Tomoyo  Ito <tmyooo73@gmail.com>
2018-10-10 20:48:41 +0900

    Implement group create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,14 +5,16 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: "グループを作成しました。"
+    else
+      render: new
+    end
   end
 
-  def edit
+  private
+  def group_params
+    params.require(:group).permit(:name, {:user_ids => [] })
   end
-
-  def update
-  end
-end
+  ends
