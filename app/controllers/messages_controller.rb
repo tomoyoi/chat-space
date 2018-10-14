fb88992b156e981c092120011c36cdf96839359f
Tomoyo  Ito <tmyooo73@gmail.com>
2018-10-14 17:48:03 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,3 +16,14 @@ class MessagesController < ApplicationController
       render :index
     end
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+end
