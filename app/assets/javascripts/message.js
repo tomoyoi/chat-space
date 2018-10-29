7589c23dc8716994ae41883966a8757b59277af8
Tomoyo  Ito <tmyooo73@gmail.com>
2018-10-29 09:44:08 +0900

    Ajaxでcreate呼び出し、処理

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,16 @@
 $(function(){
   $('#new_content').on("submit", function(e){
     e.preventDefault();
+    console.log(this);
     var FormData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: FormData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
   })
 })
