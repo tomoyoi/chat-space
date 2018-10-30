d8b21aa0124d2ff39b7128f020b0df32e34100f6
Tomoyo  Ito <tmyooo73@gmail.com>
2018-10-30 19:45:42 +0900

    調整

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,14 +10,14 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-        format.html { redirect_to group_message_path(@group), notice: "メッセージが送信されました"}
+        format.html { redirect_to group_messages_path(@group), notice: "メッセージが送信されました"}
         format.json
       end
       else
         @messages = @group.messages.includes(:user)
         flash.now[:alert] = 'メッセージを入力してください。'
+        render :index
       end
-      render :index
     end
   end
 
