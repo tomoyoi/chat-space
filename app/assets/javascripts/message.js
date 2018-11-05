80388b4628741c52aa893e0b72a112e89aeac3f9
Tomoyo  Ito <tmyooo73@gmail.com>
2018-11-05 21:41:17 +0900

    非同期　修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,50 @@
-$(function(){
-
-  function buildHTML(message){
-    var html = `<div class="message">
+$(document).on("turbolinks:load", function(){
+  function buildHTML(messages){
+    var message_html = (messages.content) ? `${messages.content}` : "";
+    var image_html = (messages.image) ? `<img class="lower-message__image" src=${ messages.image }>` : "";
+    var html = `<div class="messages">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
-                      ${message.user.name}
+                      ${messages.user_name}
                     </div>
                     <div class="upper-message__date">
-                      ${message.created_at.strtime('%Y/%m/%d %H:%M')}
+                      ${messages.datetime}
                     </div>
                   </div>
                   <div class="lower-message">
                     <div class="lower-message__contents">
-                      ${message.content}
+                      ${ message_html }
                     </div>
-                    <img class="lower-message__image" src="${message.image.url}">
+                    ${ image_html }
                   </div>
                 </div>`
-    return html
+    return html;
   }
   $('#new_content').on("submit", function(e){
     e.preventDefault();
-    var FormData = new FormData(this);
-    var url = $(this).attr('action')
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    console.log(url);
     $.ajax({
       url: url,
       type: "POST",
-      data: FormData,
+      data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false,
+      contentType: false
     })
     .done(function(data) {
       var html = buildHTML(data);
-      $('.message').append(html);
+      console.log(html);
+      $('.main_contents__content').append(html);
       $('.form__submit').prop('disabled',false);
-      $('.message').animate({ scrollTop: $('.upper-message').offset().top}, 100 );
-      $('#new_message')[0].reset();
+      $('.main_contents__content').animate({scrollTop: $('.main_contents__content')[0].scrollHeight},'fast');
+      $('#new_content')[0].reset();
     })
     .fail(function(){
       alert('error');
+      $('.form__submit').prop('disabled', false);
     })
-  })
-})
+  });
+});
+
